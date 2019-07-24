db50b2183bc58d64a047dfdb164cbc9366421995
zhengnaizhong <1014453828@qq.com>
2019-07-24 18:10:58 +0800

    Initial commit

---
+++ src/main/java/com/cw/shell/cwshellserver/CwShellServerApplication.java
@@ -0,0 +1,13 @@
+package com.cw.shell.cwshellserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwShellServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwShellServerApplication.class, args);
+    }
+
+}
